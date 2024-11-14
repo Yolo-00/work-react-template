26a0649e94ed88347979af8fd41391577f632a05
Yolo-00 <Yolo-00@users.noreply.github.com>
2024-11-14 06:26:08 +0000

    deploy: 7ab7d6395d8bba8bc59782ebfac549fe65d7098d

--- assets/js/index-B8jeJM0y.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./vendor-4tImx5aJ.js";function r(){return e.jsx("h1",{children:"menu-2-1"})}export{r as default};
---
+++ assets/js/index-BHCNbimk.js
@@ -0,0 +1,1 @@
+import{j as e}from"./vendor-DVulRf9g.js";function r(){return e.jsx("h1",{children:"Menu1"})}export{r as default};
