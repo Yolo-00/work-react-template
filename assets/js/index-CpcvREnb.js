36ddda3345c82ede902954a41f3112c88124d538
Yolo-00 <Yolo-00@users.noreply.github.com>
2024-11-15 06:46:10 +0000

    deploy: 015ebf02c2498fbd40acbdf5023453ca71ebef12

--- assets/js/index-CpcvREnb.js
+++
@@ -1,1 +0,0 @@
-import{j as e,B as t}from"./vendor-DNNUMqzz.js";import{s as r}from"./index-CALvCbN0.js";function o(){return e.jsxs(e.Fragment,{children:[e.jsx("h1",{children:"Home"}),e.jsx(t,{type:"primary",children:"Home Button"}),e.jsx(t,{onClick:()=>r(!0),children:"模仿请求token失效"}),e.jsx("div",{style:{height:"100vh"}}),e.jsx("h1",{children:"这是首页"})]})}export{o as default};
