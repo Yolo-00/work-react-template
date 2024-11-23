51f9ee5cc3d69886ea6d8427db2f4307b5fa542f
Yolo <g20180630105@gmail.com>
2024-11-23 15:55:55 +0800

    feat: 尝试组件样式覆盖并解决样式污染

--- src/views/components/uploadFile/index.tsx
+++ src/views/components/uploadFile/index.tsx
@@ -1,12 +1,13 @@
 import FileUpload from "@/components/FileUpload";
 import { useState } from "react";
+import "./app.scss";
 
 function UploadFile() {
 	const [state, setState] = useState("https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg");
 	return (
-		<>
+		<div  className="custom-upload-file-container">
 			<FileUpload setValue={setState} value={state} limit={2} />
-		</>
+		</div>
 	);
 }
 
