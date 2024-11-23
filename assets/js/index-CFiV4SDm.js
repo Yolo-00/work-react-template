73ab6022d5d8ed440d01eee87d2cacb39351a504
Yolo-00 <Yolo-00@users.noreply.github.com>
2024-11-23 07:37:20 +0000

    deploy: 1b91f29458a8875c83be9370b7951d3c17075cd5

--- assets/js/index-CFiV4SDm.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./vendor-BVX_8YO6.js";function r(){return e.jsx("h1",{children:"menu-2-1"})}export{r as default};
--- assets/js/index-DJ4pxwGX.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./vendor-BVX_8YO6.js";function r(){return e.jsx("h1",{children:"Menu1"})}export{r as default};
